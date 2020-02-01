1005749fab4e333e8655b65b1ecd798541b51eed
Andreas Gruenbacher <agruenba@redhat.com>
2020-02-01 17:17:42 +0100

    rastertoptch: Remove old C compatibility code
    We should be safe to assume that headers like <string.h> and <stdbool.h> are
    available these days.  Remove all the related autoconf tests and compatibility
    code.  If ptouch-driver should still need to be build on systems that don't
    provide that functionality, we should use gnulib to emulate the standard
    interfaces for us instead of working around it in the code.

--- rastertoptch.c
+++ rastertoptch.c
@@ -315,31 +315,7 @@
 #include <cups/raster.h>
 #include <cups/cups.h>
 #include <limits.h>
-
-#if STDC_HEADERS
-# include <string.h>
-#else
-# if !HAVE_MEMCPY
-#  define memcpy(d, s, n) bcopy ((s), (d), (n))
-# endif
-#endif
-#if HAVE_STDBOOL_H
-# include <stdbool.h>
-#else
-# if ! HAVE__BOOL
-#  ifdef __cplusplus
-typedef bool _Bool;
-#  else
-typedef unsigned char _Bool;
-#  endif
-# endif
-# define bool _Bool
-# define false 0
-# define true 1
-# define __bool_true_false_are_defined 1
-#endif
-
-
+#include <stdbool.h>
 
 #ifdef DEBUG
 #include <sys/times.h>
